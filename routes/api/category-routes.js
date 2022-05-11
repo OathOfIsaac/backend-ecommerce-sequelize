77013d0ebf0540c48c746e2767b4d3f36cbce485
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-11 12:07:00 -0500

    forgot to add foreignkeys to each route in models/index.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,48 +4,30 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   Category.findAll({
     include: {
       model: Product,
-      attributes: [
-        'id',
-        'price',
-        'stock',
-        'category_id'
-      ]
+      attributes: ['id', 'product_name','price', 'stock', 'category_id']
     }
   })
-  .then(categoryData => {
-    if(!categoryData) {
-      res.status(404).json({message: 'No categories found'});
-      return;
-    }
-    res.json(categoryData);
-  })
+  .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
-  });
+    console.log(err)
+    res.status(500).json
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
-    where: {
-      id: req.params.id
+    where: {id: req.params.id
     },
-    include: {
-      model: Product,
-      attributes:[
-        'id',
-        'price',
-        'stock',
-        'category_id'
-      ]
-    }.then(categoryData => {
+  include: {
+    model: Product,
+    attributes:["id" , "price", "stock", "category_id"]
+    }
+    .then(categoryData => {
       if(!categoryData) {
         res.status(404).json({message: 'No categories found'});
         return;
