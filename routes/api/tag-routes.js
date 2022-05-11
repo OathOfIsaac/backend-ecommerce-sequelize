79e3c2a208da5ccce1f2289ddb818882f9fde40e
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-05-11 17:44:14 -0500

    FINAL MVP

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,7 +34,14 @@ router.get('/:id', (req, res) => {
     },
     include: {
       model: Product,
-      attributes: ['id','product_name', 'price', 'stock', 'category_id']
+      attributes: 
+    [
+      'id',
+      'product_name', 
+      'price', 
+      'stock', 
+      'category_id'
+    ]
     }
   })
   .then(dbTagData => {
